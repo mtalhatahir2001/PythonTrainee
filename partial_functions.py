b89f0371aa2c2894f17e848632a5309d7fb3884d
Muhammad Talha Tahir <talhatahir@pk.folio3.com>
2023-03-06 11:51:11 +0500

    Exercise on partial functions

---
+++ partial_functions.py
@@ -0,0 +1,7 @@
+#Following is the exercise, function provided:
+from functools import partial
+def func(u, v, w, x):
+    return u*4 + v*3 + w*2 + x
+#Enter your code here to create and print with your partial function
+partial_function = partial(func, 3, 4, 6)
+print(partial_function(24))
