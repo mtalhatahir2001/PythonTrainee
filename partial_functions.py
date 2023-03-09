4bc8db2ce652517a65526ce90deaa9c93a11675a
Muhammad Talha Tahir <talhatahir@pk.folio3.com>
2023-03-09 22:08:47 +0500

    Added type hints

--- partial_functions.py
+++ partial_functions.py
@@ -1,6 +1,6 @@
 #Following is the exercise, function provided:
 from functools import partial
-def func(u, v, w, x):
+def func(u: int, v: int , w: int, x: int) -> int:
     return u*4 + v*3 + w*2 + x
 #Enter your code here to create and print with your partial function
 partial_function = partial(func, 3, 4, 6)
