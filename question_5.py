f7acf6901586cf7b18cb445656efe196852a05c8
Muhammad Talha Tahir <talhatahir@pk.folio3.com>
2023-03-12 14:15:43 +0500

    created __result_params that has all the params that are only allowed in result. Also created __air_quality_types that is for mapping number to discriptions. __fetch now returns result after downing all the filtering

--- question_5.py
+++ question_5.py
@@ -4,20 +4,42 @@ import requests
 
 
 class WeatherForecaster:
-    def __init__(self):
+    def __init__(self) -> None:
         self.__BASE_URL = "http://api.weatherapi.com/v1"
         self.__API_KEY = "e38e088af196452e88f145935230903"
         self.__CURRENT_WEATHER_ROUTE = "/current.json"
         self.__accepted_params = ["hour", "city", "postal_code"]
-        self.__priority = ["postal_code", "city", "country"]
+        self.__priority_of_q = ["postal_code", "city"]
+        self.__result_params = [
+            "feelslike_f",
+            "gust_mph",
+            "humidity",
+            "pressure_in",
+            "cloud",
+            "vis_km",
+            "temp_f",
+        ]
+        # Since weather api is only returning 1,2,3...
+        # So created this hashtable for maping.
+        self.__air_quality_types = {
+            1: "Good",
+            2: "Moderate",
+            3: "Unhealthy for sensitive group",
+            4: "Unhealthy",
+            5: "Very Unhealthy",
+            6: "Hazardous",
+        }
 
     def __create_url(self, params: dict[str, Any]) -> str:
+        """
+        Takes all the params and returns the url that can directly be fetched.
+        """
         url = f"{self.__BASE_URL}{self.__CURRENT_WEATHER_ROUTE}?key={self.__API_KEY}"
         q = self.__get_value_of_q(params)
         if q != None:
             url += f"&q={q}"
         for i in params:
-            if not i in self.__priority:
+            if not i in self.__priority_of_q:
                 url += f"&{i}={params[i]}"
         return url
 
@@ -26,13 +48,27 @@ class WeatherForecaster:
         Since q can have more then one value, this function resolves parameters into q
         based on priority. postal_code > city > country
         """
-        for i in self.__priority:
+        for i in self.__priority_of_q:
             if params.get(i) != None:
                 return params.get(i)
         return None
 
     def __fetch(self, url: str) -> dict:
-        return requests.get(url).json()
+        """
+        This module/function fetch the api also filter the result based on self.__result_params
+        """
+        response = requests.get(url).json()
+        result = dict()
+        result["date_time"] = response.get("location").get("localtime")
+        result["condition"] = response.get("current").get("condition").get("text")
+        for i in response.get("current"):
+            if i in self.__result_params:
+                result[i] = response.get("current").get(i)
+        air_quality_index = (
+            response.get("current").get("air_quality").get("us-epa-index")
+        )
+        result["air_quality"] = self.__air_quality_types.get(air_quality_index)
+        return result
 
     def get_current_weather(
         self, air_quality: bool = False, **extra_params: dict[str, Any]
@@ -60,7 +96,13 @@ class WeatherForecaster:
         filtered_params["aqi"] = "yes" if air_quality else "no"
 
         url = self.__create_url(filtered_params)
-        print(self.__fetch(url))
+        return self.__fetch(url)
 
 
-WeatherForecaster().get_current_weather(True, hour=4, city="lahore", postal_code="SW1")
+print(
+    WeatherForecaster().get_current_weather(
+        True, hour=4, city="lahore", postal_code="SW1"
+    )
+    # this postal_code is of london even though city is lahore it
+    # will return result of london since postal_code has higher priority.
+)
